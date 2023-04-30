c2d0ffd9882ab6682ac001fe44c71cfcd707638d
cbrwx <81207429+cbrwx@users.noreply.github.com>
2023-04-30 09:15:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from p2p_network import P2PNode
 from blockchain import Blockchain
 from consensus import Consensus
 from your_ai_module import YourAIModel
+from masternode import Masternode
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: main.py [node_port]")
+    if len(sys.argv) != 3:
+        print("Usage: main.py [node_port] [masternode_flag]")
         sys.exit(1)
 
     node_port = int(sys.argv[1])
+    is_masternode = sys.argv[2].lower() == "true"
 
     # Initialize the blockchain and consensus algorithm
     blockchain = Blockchain()
@@ -25,15 +27,24 @@ def main():
     p2p_node = P2PNode(port=node_port)
     p2p_node.start()
 
-    # Initialize and run the miner
-    miner = Miner(p2p_node, blockchain, ai_model)
-    miner_thread = threading.Thread(target=miner.run)
-    miner_thread.start()
+    if is_masternode:
+        # Initialize and run the masternode
+        masternode = Masternode(ai_model, p2p_node)
+        masternode_thread = threading.Thread(target=masternode.run)
+        masternode_thread.start()
 
-    # Wait for the miner thread to finish (which it won't in normal operation)
-    miner_thread.join()
+        # Wait for the masternode thread to finish (which it won't in normal operation)
+        masternode_thread.join()
+    else:
+        # Initialize and run the miner
+        miner = Miner(p2p_node, blockchain, ai_model)
+        miner_thread = threading.Thread(target=miner.run)
+        miner_thread.start()
+
+        # Wait for the miner thread to finish (which it won't in normal operation)
+        miner_thread.join()
 
 if __name__ == "__main__":
     main()
 
-    print("python main.py [node_port]")
+    print("python main.py [node_port] [masternode_flag]")
